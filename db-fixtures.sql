ab852b3765abef776c66998a20407d635a3c0991
Martin Sucha <martin.sucha@fmph.uniba.sk>
2014-07-26 18:34:47 +0200

    Pridane opravnenia do fixtures

--- db-fixtures.sql
+++ db-fixtures.sql
@@ -1,1 +1,5 @@
 INSERT INTO osoba (login, meno, priezvisko, cele_meno, vyucujuci) VALUES ('vinar1', 'Tomáš', 'Vinař', 'Mgr. Tomáš Vinař, PhD.', true);
+
+INSERT INTO ilsp_opravnenia (osoba, organizacna_jednotka, je_admin, je_garant)
+SELECT o.id, 'FMFI', true, false
+FROM osoba o WHERE o.login IN ('vinar1');
